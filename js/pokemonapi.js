9e5f1b34195b5f93dc6642cb3910084b8080a6b9
x1-lreyes <33849865+x1-lreyes@users.noreply.github.com>
2023-07-12 12:55:00 -0700

    refactored JavaScript code in pokemonapi.js file

--- js/pokemonapi.js
+++ js/pokemonapi.js
@@ -1,97 +1,100 @@
-// Pokemon API
+// Summoning Pokemon
 
-//Variables
 const summonPokemon = document.querySelector('#pokemonImg');
-const pokemonNameInput = document.querySelector('.pokemonNameInput')
-const pokemonSummonButton = document.querySelector('.pokemonSummonButton')
+const pokemonNameInput = document.querySelector('.pokemonNameInput');
+const pokemonSummonButton = document.querySelector('.pokemonSummonButton');
 
-//Summon Pokemon
 pokemonSummonButton.addEventListener('click', () => {
-    pokemonName = pokemonNameInput.value;
-    getPokemonByName(name);
-})
-
-function getPokemonByName(name) {
-    const base_url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
-
-fetch(base_url + name)  
-.then( res => {
-    return res.json();
-}).then( (facts) => {
-    summonPokemon.innerHTML = '';
-    // console.log(facts);
-    const displayName = document.createElement('h2');
-    displayName.innerText = facts.name;
-    summonPokemon.appendChild(displayName);
-    const displayId = document.createElement('h3');
-    displayId.innerText = facts.id;
-    summonPokemon.appendChild(displayId);
-
-    let existingObject = facts.types[1];
-    // console.log(existingObject);
-    if (typeof existingObject != 'undefined') {
-        const displayType1 = document.createElement('p');
-        displayType1.innerText = facts.types[0].type.name;
-        summonPokemon.appendChild(displayType1);
-        const displayType2 = document.createElement('p');
-        displayType2.innerText = facts.types[1].type.name;
-        summonPokemon.appendChild(displayType2);    
-    } else {
-        const displayType1 = document.createElement('p');
-        displayType1.innerText = facts.types[0].type.name;
-        summonPokemon.appendChild(displayType1);    
+    const pokemonName = pokemonNameInput.value;
+    getPokemonByName(pokemonName);
+});
+  
+pokemonNameInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        const pokemonName = pokemonNameInput.value;
+        getPokemonByName(pokemonName);
     }
+});
+  
+async function getPokemonByName(name) {
+    try {
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const pokemonData = await response.json();
 
-    const displayImage = document.createElement('img');
-    summonPokemon.appendChild(displayImage);
-    displayImage.src = facts.sprites.front_default;
-    displayImage.width = '300';
-    displayImage.alt = ''
-}).catch( err => console.log(err)); 
+        summonPokemon.innerHTML = '';
+        createAndAppendElement('h2', pokemonData.name);
+        createAndAppendElement('h3', pokemonData.id);
+
+        for (let type of pokemonData.types) {
+            createAndAppendElement('p', type.type.name);
+        }
+
+        const displayImage = createAndAppendElement('img');
+        displayImage.src = pokemonData.sprites.front_default;
+        displayImage.width = '300';
+        displayImage.alt = '';
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+function createAndAppendElement(tagName, text) {
+    const element = document.createElement(tagName);
+    if (text) {
+        element.innerText = text;
+    }
+    summonPokemon.appendChild(element);
+    return element;
 }
 
 
-//Variables
+//Displaying a list of Pokemon names
+
 const listLengthInput = document.querySelector('.listLengthInput');
 const offsetInput = document.querySelector('.offsetInput');
 const pokemonListButton = document.querySelector('.pokemonListButton');
 const pokemonList = document.querySelector('#pokemonList');
 
-//Get Pokemon Name List
-pokemonListButton.addEventListener('click', () => {
-    listLength = listLengthInput.value;
-    offset = offsetInput.value;
-    getPokemonList();
-})
+pokemonListButton.addEventListener('click', getPokemonList);
+listLengthInput.addEventListener('keypress', handleKeyPress);
+offsetInput.addEventListener('keypress', handleKeyPress);
 
-function getPokemonList() {
-    const base_url = `https://pokeapi.co/api/v2/pokemon?limit=${listLength}&offset=${offset}`;
-    
-    fetch(base_url)
-    .then(res => res.json() )
-    .then(data => {
-        pokemonList.innerHTML = '';
-        // console.log(data);
-        data.results.forEach( pokemon => {
-            const pokeName = document.createElement('p');
-            pokeName.innerText = pokemon.name;
-            pokemonList.appendChild(pokeName);
-        });
-    });
+function handleKeyPress(event) {
+    if (event.key === 'Enter') {
+        getPokemonList();
+    }
 }
+
+async function getPokemonList() {
+    const listLength = listLengthInput.value;
+    const offset = offsetInput.value;
+    const base_url = `https://pokeapi.co/api/v2/pokemon?limit=${listLength}&offset=${offset}`;
+
+    try {
+        const response = await fetch(base_url);
+        const data = await response.json();
+
+        pokemonList.innerHTML = '';
+
+        data.results.forEach(pokemon => {
+        const pokeName = document.createElement('p');
+        pokeName.innerText = pokemon.name;
+        pokemonList.appendChild(pokeName);
+        });
+    } catch (err) {
+        console.log(err);
+    }
+}
+
 
 //Back-to-Top button
 const btn = $('#button');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 100) {
-    btn.addClass('show');
-  } else {
-    btn.removeClass('show');
-  }
+    btn.toggleClass('show', $(window).scrollTop() > 100);
 });
 
 btn.on('click', function(e) {
-  e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '100');
+    e.preventDefault();
+    $('html, body').animate({scrollTop: 0}, 100);
 });
